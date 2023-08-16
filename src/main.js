e27b89aaa6bd85c3a7b5933b510ca47a26ad40be
niusz <163yourba@163.com>
2023-08-16 09:42:26 +0800

    feat✨: 导入了svg

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 
+// 初始化样式表
+import '@/styles/index.scss'
+// 导入 svgIcon
+import installIcons from '@/icons'
+
 const app = createApp(App)
 installElementPlus(app)
+installIcons(app)
 app.use(store).use(router).mount('#app')
